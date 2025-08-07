791043318decbb7e7ddf00fe0cfbac3005abd8c5
Pranav Vedula <vedulapranav@gmail.com>
2025-08-07 10:59:06 +0530

    Update FriendFunctionHospital.cpp

--- FriendFunctionHospital.cpp
+++ FriendFunctionHospital.cpp
@@ -1,3 +1,14 @@
+
+/* A friend function in C++ is a non-member function that is given special access to the private and protected members of a class.
+It is not defined inside the class, but it is declared using the friend keyword inside the class. */
+
+
+/* When to Use Friend Functions:
+To allow external functions or other classes to access private data without breaking encapsulation.
+When you want to perform operations involving multiple classes or objects that require internal access. */
+
+
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -28,6 +39,7 @@ void viewMedicalRecord(Patient p) {
     cout << "Age            : " << p.age << endl;
     cout << "Gender         : " << p.gender << endl;
     cout << "Medical History: " << p.medicalHistory << endl;
+    if(p.age>60) cout<< "Senior Citizen Discount applicable 10%" << endl;
     cout << "----------------------------------" << endl;
     cout << "**********************************" << endl;
 
